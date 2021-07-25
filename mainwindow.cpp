309a752e73f8627be04b7b59c18e3148f2a6caa7
Blinc <65546512+Blinc13@users.noreply.github.com>
2021-07-25 21:18:12 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::Ui_MainWindow)
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::Ui_MainWindow)
 {
     ui->setupUi(this);
 }
